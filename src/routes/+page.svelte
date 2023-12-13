2597614b22dc67bd9d3a297c452a11e56738e899
sahir <sahirde@gmail.com>
2023-12-14 00:28:07 +0100

    file saving and loading!!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,36 @@
     
     $: rootNodes = findRootNodes(nodes);
 
+    //save to disk
+    function saveMap() {
+        const data = JSON.stringify(nodes);
+        const blob = new Blob([data], {type: 'text/plain'});
+        const e = document.createEvent('MouseEvents'),
+        a = document.createElement('a');
+        a.download = "map.hierarchical";
+        a.href = window.URL.createObjectURL(blob);
+        a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
+        e.initMouseEvent('click', true, false, window,
+        0, 0, 0, 0, 0, false, false, false, false, 0, null);
+        a.dispatchEvent(e);
+    }
 
+    //load from disk 
+    function loadMap() {
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.onchange = e => { 
+            const file = e.target.files[0]; 
+            const reader = new FileReader();
+            reader.readAsText(file,'UTF-8');
+            reader.onload = readerEvent => {
+                const content = readerEvent.target.result;
+                const data = JSON.parse(content);
+                nodeList.set(data);
+            }
+        }
+        input.click();
+    }
 
 </script>
 <div class="bg-neutral-800 min-h-screen absolute min-w-full flex ">
@@ -37,10 +66,10 @@
         <p class="text-center px-12 text-sm mt-2"> Tip - use colors as a second organizational dimension.</p>
         <p class="text-center px-12 text-sm "> Tip - add "sideways" for faster categories.</p>
         <div class="mx-6 pt-6 gap-2 flex justify-between">
-            <button class="px-2 py-4 bg-blue-200 hover:bg-blue-300 w-full">
+            <button class="px-2 py-4 bg-blue-200 hover:bg-blue-300 w-full" on:click={saveMap}>
                 Save Map
             </button>
-            <button class="px-2 py-4 bg-blue-200 hover:bg-blue-300 w-full">
+            <button class="px-2 py-4 bg-blue-200 hover:bg-blue-300 w-full" on:click={loadMap}>
                 Load Map
             </button>
         </div>
